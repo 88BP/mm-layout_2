eecc8a6345b05d99bda1ebd0235c166e907494ab
Kate Shirley <kshirley@88brandpartners.com>
2016-03-10 19:29:56 -0600

    title scene

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ console.log ("hey girl heyyyyyyyy it's your javascript file here to make shit in
 
 var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onLeave"}});
 
+TweenMax.set(".st1", {scaleY: 0})
+
 
 
 // MENU TRIGGERS //
@@ -42,10 +44,99 @@ function pathPrepare ($el) {
 	var $dot = $("path#dot");
 	var $verbatim = $("path#verbatim");
 
+	var $title1 = $("#nothingTitle1")
+	var $title2 = $("#nothingTitle2")
+	var $title3 = $("#nothingTitle3")
+	var $title4 = $("#nothingTitle4")
+	var $title5 = $("#nothingTitle5")
+	var $title6 = $("#nothingTitle6")
+	var $title7 = $("#nothingTitle7")
+	var $title8 = $("#nothingTitle8")
+	var $title9 = $("#nothingTitle9")
+	var $title10 = $("#nothingTitle10")
+	var $title11 = $("#nothingTitle11")
+	var $title12 = $("#nothingTitle12")
+	var $title13 = $("#nothingTitle13")
+	var $title14 = $("#nothingTitle14")
+	var $title15 = $("#nothingTitle15")
+	var $title16 = $("#nothingTitle16")
+	var $title17 = $("#nothingTitle17")
+	var $title18 = $("#nothingTitle18")
+	var $title19 = $("#nothingTitle19")
+	var $title20 = $("#nothingTitle20")
+
+
 	// prepare SVG
 	pathPrepare($word);
 	pathPrepare($dot);
 	pathPrepare($verbatim);
+	pathPrepare($title1);
+	pathPrepare($title2);
+	pathPrepare($title3);
+	pathPrepare($title4);
+	pathPrepare($title5);
+	pathPrepare($title6);
+	pathPrepare($title7);
+	pathPrepare($title8);
+	pathPrepare($title9);
+	pathPrepare($title10);
+	pathPrepare($title11);
+	pathPrepare($title12);
+	pathPrepare($title13);
+	pathPrepare($title14);
+	pathPrepare($title15);
+	pathPrepare($title16);
+	pathPrepare($title17);
+	pathPrepare($title18);
+	pathPrepare($title19);
+	pathPrepare($title20);
+
+
+
+	new TimelineMax()
+		.add(TweenMax.to($title1, 1, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title2, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title3, 1.5, {strokeDashoffset: 0, ease:Linear.easeNone}))
+		new TimelineMax() 
+		.add(TweenMax.to($title4, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone}))
+		new TimelineMax() 
+		.add(TweenMax.to($title5, 2, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title6, 2.5, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title7, .5, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title8, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title9, 1, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title10, 1.5, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title11, 1.8, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title12, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone}))
+		new TimelineMax() 
+		.add(TweenMax.to($title13, 1.8, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title14, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title15, 2, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title16, 1.1, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title17, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title18, 1.3, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title19, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) 
+		new TimelineMax()
+		.add(TweenMax.to($title20, 2.0, {strokeDashoffset: 0, ease:Linear.easeNone}))
+		.add(TweenMax.to(".nothing", .3, {scaleY:0})) 
+		.add(TweenMax.to(".st1", .5, {scaleY:1})) 
+
+
 
 
 	// build tween
